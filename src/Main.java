2993a5272a384cc9eff7bb692f28aa68dc82304a
Parmeet Gill <parmeetgill422@gmail.com>
2025-04-11 17:03:56 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,32 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         NumberFormat currency = NumberFormat.getCurrencyInstance();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter deposit amount($):");
+        System.out.print("Enter deposit amount ($): ");
         double principal = scanner.nextDouble();
 
-        System.out.println("Enter interest rate (e.g. 7.25%:");
-        double interestrate = scanner.nextDouble() /100;
+        System.out.print("Enter interest rate (e.g. 7.25): ");
+        double interestrate = scanner.nextDouble();
 
-        System.out.println("Enter number of years: ");
+        System.out.print("Enter number of years: ");
         int years = scanner.nextInt();
 
         int dayperyear = 365;
-        double dailyrate = interestrate / dayperyear;
+        double dailyrate = (interestrate / 100) / dayperyear;
         int totaldays = dayperyear * years;
 
         double futurevalue = principal * Math.pow(1 + dailyrate, totaldays);
         double totalinterest = futurevalue - principal;
 
         System.out.println("Future Value: " + currency.format(futurevalue));
-        System.out.println("Total interest Earned: " + currency.format(totaldays));
+        System.out.println("Total Interest Earned: " + currency.format(totalinterest));
 
-        if (interestrate < 4);
-        System.out.println("The average rate of a CD is 4.25%\nI would recommend looking online for better rates.");
-
+        if (interestrate < 4) {
+            System.out.println("The average rate of a CD is 4.25%");
+            System.out.println("I would recommend looking online for better rates.");
         }
-
     }
+}
